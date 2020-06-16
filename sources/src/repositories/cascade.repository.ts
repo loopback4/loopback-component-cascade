9d6f0686554e8ae5fd135d10d0468d6276e3b4be
KoLiBer <koliberr136a1@gmail.com>
2020-06-16 18:52:32 +0430

    feat: add empty cascade methods

--- sources/src/repositories/cascade.repository.ts
+++ sources/src/repositories/cascade.repository.ts
@@ -1,13 +1,14 @@
-import { InvocationContext } from "@loopback/context";
 import {
     juggler,
     Class,
-    DefaultCrudRepository,
     Entity,
+    DefaultCrudRepository,
+    DataObject,
     Options,
     Filter,
     Where,
-    Fields,
+    Count,
+    EntityNotFoundError,
 } from "@loopback/repository";
 
 import { Ctor } from "../types";
@@ -57,6 +58,59 @@ export function CascadeRepositoryMixin<
             constructor(ctor: Ctor<Model>, dataSource: juggler.DataSource) {
                 super(ctor, dataSource);
             }
+
+            /**
+             * Create methods
+             */
+            async create(
+                entity: Model,
+                options?: CascadeOptions
+            ): Promise<Model> {
+                return super.create(entity, options);
+            }
+            async createAll(
+                entities: DataObject<Model>[],
+                options?: CascadeOptions
+            ): Promise<Model[]> {}
+
+            /**
+             * Update methods
+             */
+            async update(
+                entity: Model,
+                options?: CascadeOptions
+            ): Promise<void>;
+            async updateAll(
+                data: DataObject<Model>,
+                where?: Where<Model>,
+                options?: CascadeOptions
+            ): Promise<Count>;
+            async updateById(
+                id: ModelID,
+                data: DataObject<Model>,
+                options?: CascadeOptions
+            ): Promise<void>;
+            async replaceById(
+                id: ModelID,
+                data: DataObject<Model>,
+                options?: CascadeOptions
+            ): Promise<void>;
+
+            /**
+             * Delete methods
+             */
+            async delete(
+                entity: Model,
+                options?: CascadeOptions
+            ): Promise<void>;
+            async deleteAll(
+                where?: Where<Model>,
+                options?: CascadeOptions
+            ): Promise<Count>;
+            async deleteById(
+                id: ModelID,
+                options?: CascadeOptions
+            ): Promise<void>;
         }
 
         return Repository as any;
